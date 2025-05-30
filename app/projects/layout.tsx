0917f6d86b8d3ade44ab641fef60b0f77ef69959
Edward Ybarra <edwardcybarra@gmail.com>
2025-05-29 20:15:39 -0500

    feat: integrate ThemeProvider in layout components and update package dependencies

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -11,14 +11,7 @@ export default function ProjectsLayout({ children }: { children: ReactNode }) {
   return (
     <div>
       <h1 className="text-xl font-bold mb-4">BioForge</h1>
-     <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
+  {children}
     </div>
   );
 }
