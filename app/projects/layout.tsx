b733f025d49a4fdee1c5e63f47a955cd6201e752
Edward Ybarra <edwardcybarra@gmail.com>
2025-05-29 19:51:03 -0500

    feat: implement theme toggle and dropdown menu components; refactor layout to include ThemeProvider

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,5 +1,6 @@
 // /app/projects/layout.tsx
 import { ReactNode } from 'react';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata = {
   title: 'Projects',
@@ -10,7 +11,14 @@ export default function ProjectsLayout({ children }: { children: ReactNode }) {
   return (
     <div>
       <h1 className="text-xl font-bold mb-4">BioForge</h1>
-      {children}
+     <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
     </div>
   );
 }
